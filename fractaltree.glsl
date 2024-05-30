7beb344b32412d60e97c786de0800a7bb508cd04
Nezhdanov Demian <nzhd.demian@gmail.com>
2024-05-29 23:22:56 -0300

    tree v0.0.1

--- fractaltree.glsl
+++ fractaltree.glsl
@@ -11,49 +11,69 @@ uniform float u_time;
 
 
 
+float line(vec2 p, vec2 a, vec2 b){
+    vec2 pa = p-a;
+    vec2 ba = b-a;
+
+    float t = clamp(dot(pa, ba)/dot(ba,ba),0.,1.0);
+    vec2 cv = pa - ba*t;
+    
+    float d = length(cv);
+    if(distance(a, b)>2.){return 2.;}else{
+    return ( d);
+    }
+}
+
 #define pi  (3.14159265359*2.)
 #define steps  18.
-#define iResolution u_resolution
-#define iTime u_time
-#define fragCoord gl_FragCoord
-
-
-mat2 rot(float a){
-    return mat2(cos(a),-sin(a),sin(a),cos(a));
+vec2 rotateUV(vec2 uv, float rotation)
+{
+    return vec2(
+        cos(rotation) * uv.x + sin(rotation) * uv.y,
+        cos(rotation) * uv.y - sin(rotation) * uv.x
+    );
 }
-float branch(vec2 uv,float height,float size){
-vec2 branch = vec2(0,clamp(uv.y,0.,height));
-    float d = length(uv - branch);
-    return size/d;
+float branch(vec2 uv, float colorIn , int step){
 
+
+        vec2 pos = vec2(0.0);
+        float branchL = 0.1;
+        float branchStroke = 0.1;
+        uv -= pos;
+        float d = line(uv,vec2(0., -branchL/2.),vec2(0., branchL/2.) ) ;
+        d = smoothstep(branchStroke + 0.02  ,branchStroke,d);
+        float col = (d) ;
+        return col;
 }
 
 void main()
 {
 
     // Transforming the pixel coordinates to the range of [-1, 1]
-   vec2 uv = gl_FragCoord.xy/u_resolution.xy - vec2(.5,0);
-    uv.x *= u_resolution.x/u_resolution.y;
-    vec3 col;
-    float size = .0005;
-    float height = .3;
-    uv *= 3.;
-    float d = branch(uv,height,size);
-   
-    for(float i =0.;i<12.;i++){
-            uv.x = abs(uv.x);
-            uv.y -= height;
-        
-            height/=1.1;
-            uv *= rot(sin(u_time)/12.);
-            float d2 = branch(uv,height,size);
-            d = max(d,d2);
-    }
-    col+= d;
-    float D = branch(uv,height,size);
+    vec2 uv = gl_FragCoord.xy/u_resolution.xy * 2.0 - 1.0;
 
-    col+= D;
-   gl_FragColor = vec4(col,1.0);
+    uv.x *= u_resolution.x / u_resolution.y;
+
+    float distance = length(uv );
+//   phasor_whole = u_time;
+    
+    //  vec3 particleCol = vec3(0.0);
+
+    float breathValue =  abs((u_time/2.));
+// float breathValue = u_time;
+
+  float phaseVal =  abs(sin(u_time/2.));
+    //  position =  rotateUV(position, u_time/2.);
+    vec3 col = branch(uv,0.0,0) * theme_color;
+        
+    for(int i = 0; i < 6; i++){
+
+         col += branch(uv,0.0,0) * theme_color;
+         
+    }
+
+
+    gl_FragColor = vec4(col, 1.0);
 
 }
 
